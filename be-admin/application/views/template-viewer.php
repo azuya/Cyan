5f938ee05536cdf71bee9d993acbb24114c6aa88
Anders Dahlgren <anders@bobolo.se>
2013-01-31 17:42:51 +0100

    Added first sprite PSD

--- be-admin/application/views/template-viewer.php
+++ be-admin/application/views/template-viewer.php
@@ -1,1 +1,19 @@
 <?php require $site["bliss_engine"]["site_content"].'themes/'.$site["site"]["theme"].'/index.php'; ?>
+
+<?php
+// ---
+putenv("LC_ALL=sv_SE");
+setlocale(LC_ALL, 'sv_SE.utf-8', 'swedish');
+if (function_exists("bindtextdomain")) {
+	$domain = "messages";
+	
+	echo "[".APPPATH . "lang"."]<br>";
+	
+	bindtextdomain($domain, APPPATH . "/lang");
+	textdomain($domain);
+	bind_textdomain_codeset($domain, 'UTF-8');
+	echo _("Users")."<br>";
+	echo gettext("Users")."<br>";
+}
+// ---
+?>
